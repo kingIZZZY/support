e3ad9279c71d90793d6ed242a94a2d8e88462763
Albert Chen <albert.cht@gmail.com>
2025-03-21 15:47:48 +0800

    refactor: refactor console package

--- src/Facades/Artisan.php
+++ src/Facades/Artisan.php
@@ -8,9 +8,9 @@ use Hypervel\Foundation\Console\Contracts\Kernel as KernelContract;
 
 /**
  * @method static void bootstrap()
- * @method static void schedule(\Hypervel\Scheduling\Schedule $schedule)
+ * @method static void schedule(\Hypervel\Console\Scheduling\Schedule $schedule)
  * @method static void commands()
- * @method static \Hypervel\Foundation\Console\ClosureCommand command(string $signature, \Closure $callback)
+ * @method static \Hypervel\Console\ClosureCommand command(string $signature, \Closure $callback)
  * @method static void load(array|string $paths)
  * @method static \Hypervel\Foundation\Console\Contracts\Kernel addCommands(array $commands)
  * @method static \Hypervel\Foundation\Console\Contracts\Kernel addCommandPaths(array $paths)
@@ -20,8 +20,8 @@ use Hypervel\Foundation\Console\Contracts\Kernel as KernelContract;
  * @method static void call(string $command, array $parameters = [], \Symfony\Component\Console\Output\OutputInterface|null $outputBuffer = null)
  * @method static array all()
  * @method static string output()
- * @method static void setArtisan(\Hypervel\Foundation\Console\Contracts\Application $artisan)
- * @method static \Hypervel\Foundation\Console\Contracts\Application getArtisan()
+ * @method static void setArtisan(\Hypervel\Console\Contracts\Application $artisan)
+ * @method static \Hypervel\Console\Contracts\Application getArtisan()
  *
  * @see \Hypervel\Foundation\Console\Contracts\Kernel
  */
